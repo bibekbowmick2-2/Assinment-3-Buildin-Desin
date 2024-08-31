9d16c5847272f82e09d30fcf21dee220532e14a1
unknown <bibektotol@gmail.com>
2024-08-31 04:44:58 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  // theme: {
+  //   extend: {},
+  // },
+  theme: {
+    fontFamily: {
+      'manrope': ['Manrope', 'sans-serif'],
+      
+      
+    }
+  },
+  plugins: [require("daisyui")],
+};
